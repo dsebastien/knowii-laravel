c400083bd5ad80bc5e92055345916a8bc202e021
Sebastien Dubois <seb@dsebastien.net>
2024-08-30 18:52:03 +0200

    chore(build): added dumb npm scripts

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- TODO replace by SVG logo -->
   <div class="text-5xl md:text-6xl lg:text-8xl tracking-wide">
     <span class="font-bold text-white">Know</span><span className="text-primary-500 font-extrabold">ii</span>
   </div>
