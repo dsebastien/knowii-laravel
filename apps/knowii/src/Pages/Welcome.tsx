ecdcf5c20ef46370aa15313f288df067f494d42e
Sebastien Dubois <seb@dsebastien.net>
2024-09-02 16:57:23 +0200

    feat(all): implemented the welcome page

--- apps/knowii/src/Pages/Welcome.tsx
+++ apps/knowii/src/Pages/Welcome.tsx
@@ -1,7 +1,9 @@
 import {Head, Link} from '@inertiajs/react';
-import {useRoute} from "ziggy-js";
-import React from "react";
-import {useTypedPage} from "@knowii/common";
+import {useRoute} from 'ziggy-js';
+import React from 'react';
+import {useTypedPage} from '@knowii/common';
+import ApplicationLogo from "@/Components/ApplicationLogo";
+import {Button} from "primereact/button";
 
 interface WelcomePageProps {
   canLogin: boolean;
@@ -10,52 +12,57 @@ interface WelcomePageProps {
   phpVersion: string;
 }
 
-export default function Welcome({
-                                  canLogin,
-                                  canRegister,
-                                  laravelVersion,
-                                  phpVersion,
-                                }: WelcomePageProps) {
+export default function Welcome({canLogin, canRegister}: WelcomePageProps) {
   const route = useRoute();
   const page = useTypedPage();
 
   return (
     <>
       <Head title="Welcome"/>
-      <div
-        className="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-        {canLogin ? (
-          <div className="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
-            {page.props.auth.user ? (
-              <Link
-                href={route('dashboard')}
-                className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-              >
-                Dashboard
-              </Link>
-            ) : (
-              <>
-                <Link
-                  href={route('login')}
-                  className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                >
-                  Log in
-                </Link>
 
-                {canRegister ? (
-                  <Link
-                    href={route('register')}
-                    className="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                  >
-                    Register
-                  </Link>
-                ) : null}
-              </>
-            )}
+      <div className="bg-gray-50 text-black/80 full-page">
+        <header
+          className="p-4 md:p-6 lg:p-12 bg-gray-800 flex flex-col md:flex-row flex-wrap items-center justify-between">
+          <ApplicationLogo/>
+          {canLogin ? (
+            <>
+              <div className="flex flex-row">
+                <div className="flex flex-col sm:flex-row flex-wrap gap-4">
+                  {page.props.auth.user ? (
+                    <Button className="text-3xl font-mono font-bold px-8">
+                      <Link href={route('dashboard')} className="">Dashboard</Link>
+                    </Button>) : (
+                    <>
+                      <Link href={route('login')} className="">
+                        <Button className="text-3xl font-mono">
+                          Login
+                        </Button>
+                      </Link>
+                      {canRegister &&
+                        <Link href={route('register')} className=""><Button
+                          className="text-3xl font-mono font-bold px-8">Register
+                        </Button></Link>}
+                    </>)}
+                </div>
+              </div>
+            </>
+          ) : null}
+        </header>
+
+        <main className="mt-8 md:mt-12 lg:mt-16 px-8 md:px-0">
+          <div className="block flex flex-col items-center">
+            <h1 className="text-primary-500">Welcome&nbsp;👋</h1>
+            <h2 className="mt-4">Know<span className="text-primary-500">ii</span> is a place for your community's
+              Knowledge,
+              Ideas and Inspiration.</h2>
           </div>
-        ) : null}
 
-        Laravel v{laravelVersion} (PHP v{phpVersion})
+          <div className="mt-16 flex flex-row justify-center text-5xl font-bold">
+            Coming soon...
+          </div>
+        </main>
+        <footer className="">
+        </footer>
       </div>
     </>
   );
