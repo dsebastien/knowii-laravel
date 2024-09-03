71f8eec78682a6a36fa37d5ec5deeb7e54cc8a45
Sebastien Dubois <seb@dsebastien.net>
2024-09-03 20:04:49 +0200

    feat(api): added ping api endpoint

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PingController;
+
+Route::get('/v1/ping', [PingController::class, 'ping']);
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
